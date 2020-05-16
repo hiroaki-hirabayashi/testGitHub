39df701ce4c9fa58ca2a3ce92f2a7f054e506708
Hiroki Hirabayashi <hh.38063806@gmail.com>
2020-05-16 23:19:30 +0900

    Initial Commit

---
+++ testGitHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testGitHub
+//
+//  Created by 平林宏淳 on 2020/05/16.
+//  Copyright © 2020 Hiroaki_Hirabayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
